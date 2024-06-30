7408805b0afdb24c1ad0f787598da02244f0e9d1
FSK@SOREN <vovarybkin666@gmail.com>
2024-06-30 13:35:56 +1000

    More shortcuts for deletion

--- vort/controller/text_editor_controller.py
+++ vort/controller/text_editor_controller.py
@@ -291,26 +291,44 @@ class TextEditorController(QObject):
             self.ui.viewport().repaint()
 
     def handleTextInput(self, event: QKeyEvent) -> None:
+        if event.modifiers() == Qt.KeyboardModifier.ControlModifier and event.text():
+            match event.key():
+                case Qt.Key.Key_Backspace:
+                    self.text_cursor.beginEditBlock()
+                    self.text_cursor.movePosition(QTextCursor.MoveOperation.StartOfWord, QTextCursor.MoveMode.KeepAnchor)  # type: ignore
+                    self.text_cursor.deletePreviousChar()
+                    self.text_cursor.endEditBlock()
+                case Qt.Key.Key_Delete if not self.text_cursor.hasSelection():
+                    self.text_cursor.beginEditBlock()
+                    self.text_cursor.movePosition(QTextCursor.MoveOperation.EndOfWord, QTextCursor.MoveMode.KeepAnchor)
+                    self.text_cursor.deleteChar()
+                    self.text_cursor.endEditBlock()
+                case _:
+                    return
+            self.cursorPositionChanged.emit(self.text_cursor.position())
+            self.ui.viewport().repaint()
+
         if event.modifiers() in [Qt.KeyboardModifier.NoModifier, Qt.KeyboardModifier.ShiftModifier] and event.text():
-            if event.key() == Qt.Key.Key_Enter:
-                self.text_cursor.beginEditBlock()
-                self.text_cursor.insertBlock()
-                self.text_cursor.setPosition(self.text_cursor.block().position())
-                self.text_cursor.endEditBlock()
-            elif event.key() == Qt.Key.Key_Backspace:
-                self.text_cursor.beginEditBlock()
-                self.text_cursor.deletePreviousChar()
-                self.text_cursor.endEditBlock()
-            elif event.key() == Qt.Key.Key_Delete and not self.text_cursor.hasSelection():
-                self.text_cursor.beginEditBlock()
-                self.text_cursor.deleteChar()
-                self.text_cursor.endEditBlock()
-            elif event.key() == Qt.Key.Key_Escape:
-                return  # ignore
-            else:
-                self.text_cursor.beginEditBlock()
-                self.text_cursor.insertText(event.text())
-                self.text_cursor.endEditBlock()
+            match event.key():
+                case Qt.Key.Key_Enter:
+                    self.text_cursor.beginEditBlock()
+                    self.text_cursor.insertBlock()
+                    self.text_cursor.setPosition(self.text_cursor.block().position())
+                    self.text_cursor.endEditBlock()
+                case Qt.Key.Key_Backspace:
+                    self.text_cursor.beginEditBlock()
+                    self.text_cursor.deletePreviousChar()
+                    self.text_cursor.endEditBlock()
+                case Qt.Key.Key_Delete:
+                    self.text_cursor.beginEditBlock()
+                    self.text_cursor.deleteChar()
+                    self.text_cursor.endEditBlock()
+                case Qt.Key.Key_Escape:
+                    return  # ignore
+                case _:
+                    self.text_cursor.beginEditBlock()
+                    self.text_cursor.insertText(event.text())
+                    self.text_cursor.endEditBlock()
             self.cursorPositionChanged.emit(self.text_cursor.position())
             self.ui.viewport().repaint()
 
